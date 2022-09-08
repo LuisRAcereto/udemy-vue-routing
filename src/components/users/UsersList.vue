1cbb3bbeef7bde802d956e476b9f932763f27da7
Luis R. Acereto <lacereto@live.ngcsoftware.com>
2022-09-08 16:35:21 -0400

    Add existing project of course up to routing guards.

---
+++ src/components/users/UsersList.vue
@@ -0,0 +1,42 @@
+<template>
+  <button @click="confirmInput">Confirm</button>
+  <ul>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
+  </ul>
+</template>
+
+<script>
+import UserItem from './UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  methods: {
+    confirmInput() {
+      // Do something.
+      this.$router.push('/teams');
+    },
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log('UsersList Comp. beforeRouteEach');
+    console.log(to, from);
+    next();
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
